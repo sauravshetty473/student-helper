b9400abe061f71750bbdf47af92b393c3a786bea
Saurav <saurav.vs@somaiya.edu>
2022-04-10 05:48:54 +0530

    done with text to code

--- student_helper/lib/text_to_code/text_to_code.dart
+++ student_helper/lib/text_to_code/text_to_code.dart
@@ -1,10 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:student_helper/shared/appbar.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 
 
-class TextToCode extends StatelessWidget {
+class TextToCode extends StatefulWidget {
   const TextToCode({Key? key}) : super(key: key);
+
+  @override
+  State<TextToCode> createState() => _TextToCodeState();
+}
+
+class _TextToCodeState extends State<TextToCode> {
+  late TextEditingController _textEditingController;
+
+
+
+  @override
+  void initState() {
+    _textEditingController = new TextEditingController();
+    super.initState();
+  }
+
 
   @override
   Widget build(BuildContext context) {
@@ -20,23 +38,40 @@ class TextToCode extends StatelessWidget {
             Expanded(
               flex: 2,
                 child: Container(
-                  padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                  padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
                   height: 1000,
 
-                  child: Center(
-                    child: TextField(
-                      maxLines: 10,
-
-                      decoration: InputDecoration(
-                        fillColor: Colors.grey,
-                        border: InputBorder.none,
-                        filled: true,
+                  child:  Center(
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
 
 
+                      children: [
+                        TextField(
+                          controller: _textEditingController,
+                          maxLines: 10,
 
-                      ),
+                          decoration: const InputDecoration(
+                            fillColor: Colors.grey,
+                            border: InputBorder.none,
+                            filled: true,
 
-                    ),
+
+
+                          ),
+
+                        ),
+
+
+                        TextButton(onPressed: (){
+                          setState(() {
+
+                          });
+                        }, child: Text(
+                          "Convert"
+                        ))
+                      ],
+                    )
                   ),
 
             )),
@@ -109,12 +144,24 @@ class TextToCode extends StatelessWidget {
                           ),
 
 
-                          Scrollbar(
-                            child: SingleChildScrollView(
-                              child: Text(
-                                "Code example"
-                              ),
-                            ),
+                          FutureBuilder(
+                            future: registerAPI(_textEditingController.text),
+                            builder: (context, snapshot){
+                              if(snapshot.data == null){
+                                return const CircularProgressIndicator();
+                              }
+
+
+                              return Scrollbar(
+                                child: Text(
+                                  snapshot.data as String,
+                                  style: const TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 20,
+                                  ),
+                                ),
+                              );
+                            },
                           )
                         ],
                       ),
@@ -133,3 +180,37 @@ class TextToCode extends StatelessWidget {
     );
   }
 }
+
+
+ Future registerAPI(String val) async {
+const uri = 'http://127.0.0.1:8000/';
+
+var added = "check if palindrome";
+
+
+print("started");
+
+
+String modified = uri + "code/" + val.trim().replaceAll(" ", "%20");
+print(modified);
+
+
+var mid = Uri.parse(modified);
+http.Response response = await http.get(
+    mid,
+    headers: {
+
+    }
+).onError((error, stackTrace){
+  print(error);
+  return Future.delayed(Duration.zero);
+});
+
+print("hello");
+
+print(response.body);
+// ignore: avoid_print
+
+return (json.decode(response.body)["code"] as String).replaceAll("\t", "    ");
+}
+
